7fc6aec3c614d5a3fc36598af4e3e72e72a243f3
Niwash Acharya <acharyaniwash19@gmail.com>
2019-02-18 20:09:59 +0530

    Add files via upload

---
+++ typed.js/main.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    $(".typed").typed({
+        strings: ["Anonymous","Legion","Stan"],
+        startDelay:500,
+        smartBackspace:true,
+        typeSpeed:80,
+        backSpeed:50,
+        backDelay:1000,
+        loop:true,
+        loopCount:Infinity,
+        fadeOut: false,
+        fadeOutClass: 'typed-fade-out',
+        fadeOutDelay: 500,
+    })
+});
+  
